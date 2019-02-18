7278122d5e533e353576e2e1a1afd3ebe44ef309
Alex <aosivt@gmail.com>
2019-02-18 14:50:03 +0700

    fix for viewed angular app

--- src/main/java/sdek/supplier/controllers/Default.java
+++ src/main/java/sdek/supplier/controllers/Default.java
@@ -1,9 +1,10 @@
 package sdek.supplier.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+@Controller
 public class Default {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String getIndexApp(Model model) {
